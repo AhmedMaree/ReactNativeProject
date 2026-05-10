cf6387578f7d975139d7c5bfd23067aead513a67
AhmedMaree <60566202+AhmedMaree@users.noreply.github.com>
2026-05-11 01:27:45 +0300

    Add files via upload

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { TMDB_API_KEY, TMDB_BASE_URL, ENDPOINTS } from '../utils/constants';
+import { ANIME_GENRE_ID, TMDB_API_KEY, TMDB_BASE_URL, ENDPOINTS } from '../utils/constants';
 import { getItem, setItem } from '../utils/storage';
 import { STORAGE_KEYS } from '../utils/constants';
 const tmdb = axios.create({
@@ -12,23 +12,57 @@ const tmdb = axios.create({
 function extractData(response) {
   return response.data;
 }
-export async function fetchMovieList(endpoint, page = 1) {
-  const response = await tmdb.get(endpoint, {
-    params: {
-      page
-    }
+function today() {
+  return new Date().toISOString().slice(0, 10);
+}
+function buildAnimeDiscoverParams(filter, page) {
+  const params = {
+    page,
+    include_adult: false,
+    include_video: false,
+    sort_by: filter.sortBy,
+    with_genres: ANIME_GENRE_ID,
+    with_original_language: 'ja'
+  };
+  if (filter.minVotes) {
+    params['vote_count.gte'] = filter.minVotes;
+  }
+  if (filter.releaseFrom === 'today') {
+    params['primary_release_date.gte'] = today();
+  }
+  if (filter.releaseTo === 'today') {
+    params['primary_release_date.lte'] = today();
+  }
+  return params;
+}
+function isAnimeMovie(movie) {
+  return !movie.adult && movie.genre_ids?.includes(ANIME_GENRE_ID) && movie.original_language === 'ja';
+}
+function filterSafeMovies(results = []) {
+  return results.filter(isAnimeMovie);
+}
+function sanitizeMovieList(data) {
+  return {
+    ...data,
+    results: filterSafeMovies(data.results)
+  };
+}
+export async function fetchMovieList(filter, page = 1) {
+  const response = await tmdb.get(ENDPOINTS.DISCOVER, {
+    params: buildAnimeDiscoverParams(filter, page)
   });
-  return extractData(response);
+  return sanitizeMovieList(extractData(response));
 }
 export async function searchMovies(query, page = 1) {
   const response = await tmdb.get(ENDPOINTS.SEARCH, {
     params: {
       query,
       page,
-      include_adult: false
+      include_adult: false,
+      with_genres: ANIME_GENRE_ID
     }
   });
-  return extractData(response);
+  return sanitizeMovieList(extractData(response));
 }
 export async function fetchMovieDetails(movieId) {
   const cacheKey = STORAGE_KEYS.MOVIE_CACHE(movieId);
@@ -50,8 +84,9 @@ export async function fetchMovieCredits(movieId) {
 export async function fetchSimilarMovies(movieId, page = 1) {
   const response = await tmdb.get(`${ENDPOINTS.SIMILAR}/${movieId}/similar`, {
     params: {
-      page
+      page,
+      include_adult: false
     }
   });
-  return extractData(response);
+  return sanitizeMovieList(extractData(response));
 }
